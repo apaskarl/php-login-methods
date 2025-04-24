f81efc8e88c9b2d9fdf86441a746053f26b02f4e
James Karl Apas <134823154+apaskarl@users.noreply.github.com>
2025-04-24 19:03:20 +0800

    database connection setup in register.php

--- register.php
+++ register.php
@@ -1,5 +1,17 @@
 <?php
-include 'db.php';
+// Database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "login_php";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
 
 $success = '';
 $error = '';
